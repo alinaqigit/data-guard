d2c37444762c17925f446fff36879212a2ca22e8
Ali Naqi <alinaqinaqvi861@gmail.com>
2026-02-07 11:29:26 +0500

    cleaned up some code

---
+++ src/server/src/modules/auth/dto/index.ts
@@ -0,0 +1,4 @@
+export * from './CustomRequest.dto';
+export * from './login.dto';
+export * from './Parent.dto';
+export * from './register.dto';
---
+++ src/server/src/modules/auth/dto/login.dto.ts
@@ -0,0 +1,6 @@
+import { ParentDTO } from "./Parent.dto";
+
+export class loginDTO extends ParentDTO {
+  username = "username";
+  password = "password";
+}
